def5d44a47aaec0fcd6661f5862125dcd45f430d
galaxy-comet <oleksandrshkabura811@gmail.com>
2022-08-12 02:36:28 +0800

    feat: prepare themeing for project

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,17 @@ module.exports = {
   content: ['./src/**/*.{html,ts}'],
   theme: {
     extend: {},
+    colors: {
+      black: '#000000',
+      white: '#ffffff',
+      input: '#182c47',
+      card: '#0e1a2b',
+      btn: '#5692e8',
+      common: '#c1d1e8',
+    },
+    letterSpacing: {
+      widest: '.15em',
+    },
   },
   plugins: [],
 };
